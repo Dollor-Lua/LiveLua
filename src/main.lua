af963411a86983a9a766b5f719acecb9c78471f6
Dollor-Lua <dollorrblx@gmail.com>
2022-01-03 21:00:53 -0700

    commands!

--- src/main.lua
+++ src/main.lua
@@ -1,8 +1,12 @@
-local server = require("src.server.server")
+if arg[1] == "serve" then
+    local server = require("src.server.server")
 
-local serving = server.new({
-    port = '3000',
-    host = 'localhost'
-})
+    local serving = server.new({
+        port = '3000',
+        host = 'localhost'
+    })
 
-serving:start()
+    serving:start()
+elseif arg[1] == "init" then
+    -- generate folders in the current working directory
+end
